6261a74d099b7e33289706119c263117f9c9639d
Jonathan Paiva <jonathanapaiva@gmail.com>
2024-05-08 22:06:22 -0300

    feat: add validations

--- cifra.js
+++ cifra.js
@@ -3,19 +3,28 @@ const CODIGO_Z = 122;
 const alfabeto = 'abcdefghijklmnopqrstuvwxyz';
 
 /**
- * 
  * @param {string} chave 
  * @param {string} texto 
  * @returns texto cifrado
  */
 function cifrar(chave, texto) {
+  if (texto.match(new RegExp('[^a-zA-Z ]'))) {
+    console.log('Cheque seu texto!');
+    return '';
+  }
+
   let textoCifrado = '';
 
   for (let letra of texto) {
+    if (!letra.trim()) {
+      textoCifrado += letra;
+      continue;
+    }
+
     let codigo = letra.charCodeAt(0) + alfabeto.indexOf(chave);
     codigo = codigo > CODIGO_Z ? (codigo % (CODIGO_Z + 1)) + CODIGO_A : codigo;
     
-    let letraModificada = String.fromCharCode(codigo);
+    const letraModificada = String.fromCharCode(codigo);
    
     textoCifrado += letraModificada;
   }
@@ -23,16 +32,26 @@ function cifrar(chave, texto) {
   return textoCifrado;
 }
 
-console.log(cifrar('z', 'xyz'));
+console.log(cifrar('z', 'te2112#@!#@amo'));
 
+/**
+ * @param {string} chave 
+ * @param {string} textoCifrado 
+ * @returns texto decifrado
+ */
 function decifrar(chave, textoCifrado) {
   let textoDecifrado = '';
 
   for (let letra of textoCifrado) {
+    if (!letra.trim()) {
+      textoDecifrado += letra;
+      continue;
+    }
+
     let codigo = letra.charCodeAt(0) - alfabeto.indexOf(chave);
     codigo = codigo < CODIGO_A ? (CODIGO_Z + 1) - (CODIGO_A % codigo) : codigo;
    
-    let letraModificada = String.fromCharCode(codigo);
+    const letraModificada = String.fromCharCode(codigo);
    
     textoDecifrado += letraModificada;
   }
@@ -40,5 +59,4 @@ function decifrar(chave, textoCifrado) {
   return textoDecifrado;
 }
 
-
-console.log(decifrar('z', 'wxy'));
+console.log(decifrar('z', 'sd zln'));
