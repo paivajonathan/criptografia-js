294be3b42ad51dd230be1076f9e2eac7a24a154a
Jonathan Paiva <jonathanapaiva@gmail.com>
2024-05-08 21:49:01 -0300

    feat: add functions

--- cifra.js
+++ cifra.js
@@ -1,3 +1,7 @@
+const CODIGO_A = 97;
+const CODIGO_Z = 122;
+const alfabeto = 'abcdefghijklmnopqrstuvwxyz';
+
 /**
  * 
  * @param {string} chave 
@@ -5,10 +9,6 @@
  * @returns texto cifrado
  */
 function cifrar(chave, texto) {
-  const CODIGO_A = 97;
-  const CODIGO_Z = 122;
-  const alfabeto = 'abcdefghijklmnopqrstuvwxyz';
-  
   let textoCifrado = '';
 
   for (let letra of texto) {
@@ -24,3 +24,21 @@ function cifrar(chave, texto) {
 }
 
 console.log(cifrar('z', 'xyz'));
+
+function decifrar(chave, textoCifrado) {
+  let textoDecifrado = '';
+
+  for (let letra of textoCifrado) {
+    let codigo = letra.charCodeAt(0) - alfabeto.indexOf(chave);
+    codigo = codigo < CODIGO_A ? (CODIGO_Z + 1) - (CODIGO_A % codigo) : codigo;
+   
+    let letraModificada = String.fromCharCode(codigo);
+   
+    textoDecifrado += letraModificada;
+  }
+
+  return textoDecifrado;
+}
+
+
+console.log(decifrar('z', 'wxy'));
