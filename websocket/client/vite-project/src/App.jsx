a06591bafc0581ed800e98fd4869a119be6cb17c
Jonathan Paiva <jonathanapaiva@gmail.com>
2024-05-11 15:14:19 -0300

    style: update styling

--- websocket/client/vite-project/src/App.jsx
+++ websocket/client/vite-project/src/App.jsx
@@ -1,4 +1,4 @@
-import style from "./App.module.css";
+import styles from "./App.module.css";
 import { useState, useEffect, useRef } from "react";
 
 const ADDRESS = "ws://localhost:8000";
@@ -7,11 +7,12 @@ function App() {
   const [text, setText] = useState("");
   const [data, setData] = useState([]);
   const socket = useRef(null);
+  const messagesEndRef = useRef(null);
 
   function checkEvent(username, message, event) {
     switch (event) {
       case "connection":
-        return { author: "", text: `${username} entrou no servidor.`};
+        return { author: "", text: `${username} entrou no servidor.` };
       case "close":
         return { author: "", text: `${username} saiu do servidor.` };
       default:
@@ -28,7 +29,9 @@ function App() {
   useEffect(() => {
     const params = new URLSearchParams(document.location.search);
     const username = params.get("username") ?? "";
-    const ws = new WebSocket(`${ADDRESS}${ username ? "?username=" + username : username }`);
+    const ws = new WebSocket(
+      `${ADDRESS}${username ? "?username=" + username : username}`
+    );
     console.log("Use effect executado");
 
     ws.onopen = () => {
@@ -42,13 +45,13 @@ function App() {
       if (message instanceof Array) {
         message.forEach(({ username, message, event }) => {
           const newData = checkEvent(username, message, event);
-          setData(prevData => [newData, ...prevData]);
+          setData((prevData) => [...prevData, newData]);
         });
         return;
       }
 
       const newData = checkEvent(username, message, event);
-      setData(prevData => [newData, ...prevData]);
+      setData((prevData) => [...prevData, newData]);
     };
 
     socket.current = ws;
@@ -56,29 +59,34 @@ function App() {
     return () => ws.close();
   }, []);
 
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [data]);
+
   return (
-    <>
+    <main className={styles.main}>
       <h1>Cryptowhats</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={text}
-          onChange={(event) => setText(event.target.value)}
-          placeholder="Digite uma mensagem"
-        />
-        <input type="submit" value="Enviar" />
-      </form>
-      <div>
-        <h1>Messages:</h1>
+      <div className={styles.messages}>
         <ul>
           {data.map((data, index) => (
-            <li key={index}>
-              {`${data.author ?? ""}${data.text}`}
-            </li>
+            <li key={index}>{`${data.author ?? ""}${data.text}`}</li>
           ))}
         </ul>
+        <div ref={messagesEndRef} />
       </div>
-    </>
+
+      <form onSubmit={handleSubmit}>
+        <div className={styles.messageBox}>
+          <input
+            type="text"
+            value={text}
+            onChange={(event) => setText(event.target.value)}
+            placeholder="Digite uma mensagem"
+          />
+          <input type="submit" value="Enviar" />
+        </div>
+      </form>
+    </main>
   );
 }
 
