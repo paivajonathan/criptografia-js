7b4788073a7319b9e4f2fe96291eb330c2815a6e
Jonathan Paiva <jonathanapaiva@gmail.com>
2024-05-16 22:12:40 -0300

    update rsa calculation

--- index.js
+++ index.js
@@ -1,157 +1,125 @@
-/**
- *
- * @param {number} number
- * @returns result
- */
-function isPrimal(number) {
-  if (number <= 1) return false;
+function isPrime(number) {
+  if (number <= 1)
+    return false;
 
-  for (let i = 2; i < Math.sqrt(number); i++)
-    if (number % i === 0) return false;
+  for (let i = 2; i < Math.sqrt(number); i++) {
+    if (number % i === 0)
+      return false;
+  }
 
   return true;
 }
 
-/**
- *
- * @param {number} min
- * @param {number} max
- * @returns random number
- */
-function getRandomBetween(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-/**
- *
- * @param {number} maxValue
- */
 function sieve(maxValue) {
-  /**
-   * @type {boolean[]}
-   */
   const values = new Array(maxValue + 1).fill(true);
 
-  /**
-   * @type {number[]}
-   */
   const primes = [];
 
-  for (let i = 0; i < 2; i++)
-    values[i] = false;
+  for (let i = 0; i < 2; i++) values[i] = false;
 
   for (let i = 2; i < values.length; i++) {
     if (!i) continue;
+    
     for (let j = i + i; j < values.length; j += i)
       values[j] = false;
   }
 
-  for (const position in values)
-    values[position] && primes.push(+position);
+  for (const position in values) {
+    if (values[position])
+      primes.push(parseInt(position));
+  }
 
   return primes;
 }
 
-/**
- * @returns array of primal numbers
- */
-function generatePrimalPair() {
-  let prime1 = 0, prime2 = 0;
-
-  const primes = sieve(100);
-
-  while ((prime1 === prime2) || (prime1 % 6 !== 5) || (prime2 % 6 !== 5)) {
-    prime1 = primes[Math.floor(Math.random() * primes.length)];
-    prime2 = primes[Math.floor(Math.random() * primes.length)];
-  }
-
-  return [prime1, prime2];
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
 }
 
-/**
- *
- * @param {string} text
- * @returns object containing the encrypted text, and private keys
- */
+function generatePrimePairBetween(minValue, maxValue) {
+  let prime1 = 0, prime2 = 0;
+
+  const primes = sieve(maxValue).filter((n) => n >= minValue);
+
+  while (prime1 === prime2 || prime1 % 6 !== 5 || prime2 % 6 !== 5) {
+    prime1 = getRandomElement(primes);
+    prime2 = getRandomElement(primes);
+  }
+
+  return { prime1, prime2 };
+}
+
 function encrypt(text) {
   if (!text) {
     console.log("Text must not be empty!");
     return;
   }
 
-  const [privateKey1, privateKey2] = generatePrimalPair();
-  const publicKey = privateKey1 * privateKey2;
+  const MINIMUM_BLOCK = 10;
+  const MAXIMUM_BLOCK = 99;
+
+  const { prime1, prime2 } = generatePrimePairBetween(MINIMUM_BLOCK, MAXIMUM_BLOCK);
+  const publicKey = prime1 * prime2;
 
   const encryptedBlocks = [];
 
   for (const char of text) {
     const charCode = char.charCodeAt(0);
     const block = char === " " ? charCode + 67 : charCode - 55;
-    const encryptedBlock = Math.pow(block, 3) % publicKey;
+    const encryptedBlock = (block ** 3) % publicKey;
     encryptedBlocks.push(encryptedBlock);
   }
 
   const encryptedText = encryptedBlocks.join(" ");
 
-  return {
-    encryptedText,
-    privateKey1,
-    privateKey2,
-  };
+  return { encryptedText, prime1, prime2 };
 }
 
-/**
- *
- * @param {number} number
- * @param {number} mod
- * @returns modular inverse of the number
- */
 function getInverse(number, mod) {
   for (let i = 1; i < mod; i++) {
-    if ((number * i) % mod === 1) return i;
+    if ((number * i) % mod === 1)
+      return i;
   }
   return -1;
 }
 
-/**
- *
- * @param {string} encryptedText
- * @param {number} privateKey1
- * @param {number} privateKey2
- * @returns text
- */
-function decrypt(encryptedText, privateKey1, privateKey2) {
+function decrypt(encryptedText, prime1, prime2) {
   if (!encryptedText) {
     console.log("Encrypted text must not be empty!");
     return;
   }
 
-  if (isNaN(privateKey1) || isNaN(privateKey2)) {
-    console.log("Private keys must be numbers!");
+  if (isNaN(prime1) || isNaN(prime2)) {
+    console.log("Both values must be numbers!");
     return;
   }
 
-  if (!isPrimal(privateKey1) || !isPrimal(privateKey2)) {
-    console.log("Private keys must both be primal numbers!");
+  if (!isPrime(prime1) || !isPrime(prime2)) {
+    console.log("Both numbers must be prime!");
     return;
   }
-  
-  const publicKey = privateKey1 * privateKey2;
-  const totientFunction = (privateKey1 - 1) * (privateKey2 - 1);
-  const decryptingKey = getInverse(3, totientFunction);
-  
-  if (decryptingKey === -1) {
-    console.log("The keys are not valid!");
+
+  const publicKey = prime1 * prime2;
+  const totientFunction = (prime1 - 1) * (prime2 - 1);
+  const privateKey = getInverse(3, totientFunction);
+
+  if (privateKey === -1) {
+    console.log("The primes are not valid!");
     return;
   }
-  
+
   const encryptedBlocks = encryptedText.split(" ").map(Number);
   const chars = [];
-  
+
   for (const encryptedBlock of encryptedBlocks) {
-    const block = parseInt(BigInt(BigInt(encryptedBlock) ** BigInt(decryptingKey)) % BigInt(publicKey));
+    const block = parseInt(
+      BigInt( BigInt(encryptedBlock) ** BigInt(privateKey) ) %
+        BigInt(publicKey)
+    );
+
     const charCode = block === 99 ? block - 67 : block + 55;
     const char = String.fromCharCode(charCode);
+    
     chars.push(char);
   }
 
@@ -159,5 +127,5 @@ function decrypt(encryptedText, privateKey1, privateKey2) {
   return text;
 }
 
-const { encryptedText, privateKey1, privateKey2 } = encrypt("A baTata esPalhA a RAma pelo CHao");
-console.log(decrypt(encryptedText, privateKey1, privateKey2));
+const { encryptedText, prime1, prime2 } = encrypt("A baTata esPalhA a RAma pelo CHao");
+console.log(decrypt(encryptedText, prime1, prime2));
