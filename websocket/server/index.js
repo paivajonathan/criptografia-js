09ea60086814835a34a2487bb48650e00afa83f3
Jonathan Paiva <jonathanapaiva@gmail.com>
2024-05-23 01:05:03 -0300

    refactor(back): refactor functions

--- websocket/server/index.js
+++ websocket/server/index.js
@@ -7,26 +7,8 @@ const server = http.createServer();
 const wsServer = new WebSocketServer({ server });
 const port = 8000;
 
-const MAX_CONNECTIONS = 2;
 const connections = {};
 const publicKeys = {};
-
-function isNumeric(str) {
-  if (typeof str != "string") return false
-  return !isNaN(str) &&
-         !isNaN(parseFloat(str))
-}
-
-function isJSON(text) {
-  if (typeof text !== "string" || isNumeric(text)) return false;
-
-  try {
-    JSON.parse(text);
-    return true;
-  } catch (error) {
-    return false;
-  }
-}
 
 function broadcastKeys() {
   Object.keys(connections).forEach((connUsername) => {
@@ -38,8 +20,6 @@ function broadcastKeys() {
         publicKey[publicKeyUsername] = publicKeys[publicKeyUsername];
         return publicKey;
       }, {});
-
-    // if (!Object.keys(filteredKeys).length) return;
 
     const connection = connections[connUsername];
 
@@ -53,7 +33,7 @@ function broadcastKeys() {
   });
 }
 
-function broadcast(connections, username, event, message = "") {
+function broadcastMessage(connections, username, event, message = "") {
   console.log(`${username} - ${message} - ${event}`);
 
   const data = { username, message, event };
@@ -67,11 +47,6 @@ function broadcast(connections, username, event, message = "") {
 }
 
 wsServer.on("connection", (connection, request) => {
-  // if (Object.keys(connections).length >= MAX_CONNECTIONS) {
-  //   connection.close(1008, "Foi excedido o limite de conexões!");
-  //   return;
-  // }
-
   const username = url.parse(request.url, true).query.username;
 
   if (Object.keys(connections).includes(username)) {
@@ -80,31 +55,34 @@ wsServer.on("connection", (connection, request) => {
   }
 
   connections[username] = connection;
-  broadcast(connections, username, "connection");
+  broadcastMessage(connections, username, "connection");
 
   connection.on("message", (bytes) => {
-    const { username: messageUsername, message, event, destination } = JSON.parse(bytes.toString());
+    const {
+      username: messageUsername,
+      message,
+      event,
+      destination,
+    } = JSON.parse(bytes.toString());
 
     if (event === "key") {
       publicKeys[messageUsername] = message;
       broadcastKeys();
-    } else if (event === "message") {
-      if (Object.keys(connections).length < 2) {
-        connection.close(1000, "Espere algum outro usuário se conectar!");
-        return;
-      }
-
-      Object.keys(connections).forEach((connUsername) => {
-        if (destination !== connUsername) return;
-        console.log([username, message, event, destination].join(" - "));
-        const connection = connections[connUsername];
-        connection.send(JSON.stringify({ username, message, event }));
-      })
+      return;
     }
+
+    Object.keys(connections).forEach((connUsername) => {
+      if (connUsername !== destination) return;
+      
+      console.log([username, message, event, destination].join(" - "));
+      
+      const connection = connections[connUsername];
+      connection.send(JSON.stringify({ username, message, event }));
+    });
   });
 
   connection.on("close", () => {
-    broadcast(connections, username, "close");
+    broadcastMessage(connections, username, "close");
     delete connections[username];
     delete publicKeys[username];
     broadcastKeys();
@@ -112,5 +90,5 @@ wsServer.on("connection", (connection, request) => {
 });
 
 server.listen(port, () => {
-  console.log(`The server is running on port ${port}`);
+  console.log(`O servidor está rodando na ${port}!`);
 });
